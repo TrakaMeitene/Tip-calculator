b875279443221aa507dd9be3d5bd70bb559606b4
Sandra Jurberga-Šaudine <80694112+TrakaMeitene@users.noreply.github.com>
2021-10-16 15:53:11 +0300

    projekts

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import 'antd/dist/antd.css'; 
+import './App.css';
+import Logs from './components/Logs'
+function App() {
+  return (
+<div className="flex">
+<Logs />
+</div>
+  );
+}
+
+export default App;
